c5587ecc57f8c67231f80946f2489976931971dc
LLMonsta <epitts740@gmail.com>
2026-03-27 05:35:28 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FlipFlowAI from "./FlipFlowAI";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FlipFlowAI />
-  </React.StrictMode>
-);
